1d72d8e813a88367d96114bb1787143ebb15c7ff
GorbachevaPolina <gorbacheva.polina7394@gmail.com>
2023-04-13 17:18:06 +0300

    fixed temp display

--- thermostat/thermostat.ino
+++ thermostat/thermostat.ino
@@ -1,7 +1,17 @@
+#include <DallasTemperature.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
+#include <OneWire.h>
+#include <math.h>
 #include "page.h"
 #include "response.h"
+
+OneWire ds(D3);
+DallasTemperature sensors(&ds);
+
+DeviceAddress sensor1 = { 0x28, 0xFF, 0xC3, 0xF5, 0x67, 0x18, 0x03, 0x73 };
+float readSensor1 = 0;
+String readSensor1String = "";
 
 const char* ssid = "TP-Link_58B0";  // SSID
 const char* password = "31795777"; // пароль
@@ -12,7 +22,7 @@ const int relayPin = D0;
 const int thermoPin = A0;
 const int btnPin = D1;
 
-double thermoValue = 0;
+int thermoValue = 0;
 bool isOn = true;
 int prevBtnState = 1;
 String inputValue = "554";
@@ -20,13 +30,20 @@ bool isManuel = false;
 
 ESP8266WebServer server(80);
 
+float readDs18b20() {
+  sensors.requestTemperatures();
+  readSensor1 = sensors.getTempC(sensor1);
+  //Serial.println(readSensor1);
+  return readSensor1;
+}
+
 void handleRoot() {
   String s = MAIN_page;
   server.send(200, "text/html", s);
 }
 
 void handleValue() {
-  thermoValue = analogRead(thermoPin);
+  thermoValue = floor(readDs18b20());
   String value = String(thermoValue);
 
   server.send(200, "text/plane", value);
@@ -55,6 +72,7 @@ void handleToggleMode() {
 
 void setup() {
   Serial.begin(115200);
+  sensors.begin();
   // delay(100);
   pinMode(relayPin, OUTPUT);
   pinMode(btnPin, INPUT_PULLUP);
